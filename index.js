4608509e8e2dceeff7178ed140645232b67a025b
Kateattardo <youremail@domain.com>
2023-12-18 17:07:19 -0500

    main

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import morgan from "morgan";
+
+const app = express();
+
+dotenv.config();
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log("app listening on port ", port);
+});
