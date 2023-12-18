95049b487c6851219058c48a032420ed7f3e5a14
jadewang425 <jadewang425@gmail.com>
2023-12-18 17:40:52 -0500

    added config/db.js

---
+++ config/db.js
@@ -0,0 +1,15 @@
+'use strict'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const database = {
+    development: process.env.MONGODB_URI,
+    test: process.env.MONGODB_URI,
+}
+
+const localDb = process.env.TESTENV ? database.test : database.development
+
+const currentDb = process.env.MONGODB_URI || localDb
+
+export default currentDb
