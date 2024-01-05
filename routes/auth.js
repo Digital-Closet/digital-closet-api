f7b55c2549b028b4e0f4146f35078dd01be131d0
Kateattardo <youremail@domain.com>
2024-01-05 12:34:14 -0500

    reguire sign in middelware

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,16 @@ import express from "express";
 
 const router = express.Router();
 
+//middleware
+import { requireSignin } from "../middlewares/auth.js";
+
 //controllers
-import { register } from "../controllers/auth.js";
+import { register, login, secret } from "../controllers/auth.js";
 
 router.post("/register", register);
+router.post("/login", login);
+
+//testing
+router.get("/secret", requireSignin, secret);
 
 export default router;
